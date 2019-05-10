eeaef8bc6c4991ef834dc1b9bf2ef461adac051e
killerjef <jeferson12309@gmail.com>
2019-05-10 11:20:24 -0500

    respositorio prof

--- database.php
+++ database.php
@@ -1,20 +1,17 @@
 <?php
-	//credenciales de coneccion a base de datos
-	//acsedo
-	//127.0.0.1/market/database.php
-	$username= "root";
-	$servername= "localhost"; //127.0.0.1
-	$password= "";
-	$dbname= "market";
+    $servername="localhost";
+    $username="root";
+    $pass="";
+    $DBname="market";
 
-	$conn = new mysqli($servername,$username,$password,$dbname);	
+    //create connection to database
 
-	if($conn->connect_error){
-		die("Error: ". connect_error);
-	
-	}else{
-		//die("conexion exitosa a market");	
-	
-	}
+    $conn = new mysqli($servername,$username,$pass,$DBname);
+    //cheking
+    if($conn->connect_error){
+        die("Connection failed".$conn->connect_error);
+    }else{
+        //echo "i am connectes to tienda";
+    }
 
 ?>
